326ef9ada25a9d7868fae1b20bfb513dfe587222
NKulbaka <nikita.kulbaka@mail.ru>
2026-04-19 21:56:43 +0300

    Initial commit

---
+++ src/test/java/ru/auctionservice/AuctionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.auctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
